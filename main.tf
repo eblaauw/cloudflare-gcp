5b7fdc6e96ccb1b35c2db8fbc2105f23ab89d28d
Edzo <edzo@edzo.dev>
2020-12-23 12:06:54 +0400

    chore(): Add cloudflare IPs

---
+++ main.tf
@@ -0,0 +1,84 @@
+provider "google" {
+  project = var.project
+  region  = var.region
+}
+
+# ---------------------------------------------------------------------------------------------------------------------
+# CLOUD ARMOR FOR PRODUCTION APIS
+# ---------------------------------------------------------------------------------------------------------------------
+
+resource "google_compute_security_policy" "policy" {
+  name = "production-generic-policy"
+
+  rule {
+    action   = "allow"
+    priority = "998"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = [
+          "173.245.48.0/20",
+          "103.21.244.0/22",
+          "103.22.200.0/22",
+          "103.31.4.0/22",
+          "141.101.64.0/18",
+          "108.162.192.0/18",
+        ]
+      }
+    }
+    description = "Allow Cloudflare IPs - ipv4 (1)"
+  }
+  rule {
+    action   = "allow"
+    priority = "999"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = [
+          "190.93.240.0/20",
+          "188.114.96.0/20",
+          "197.234.240.0/22",
+          "198.41.128.0/17",
+          "162.158.0.0/15",
+          "104.16.0.0/12",
+          "172.64.0.0/13",
+          "131.0.72.0/22",
+        ]
+      }
+    }
+    description = "Allow Cloudflare IPs - ipv4 (2)"
+  }
+
+
+  rule {
+    action   = "allow"
+    priority = "1001"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = [
+          "2400:cb00::/32",
+          "2606:4700::/32",
+          "2803:f800::/32",
+          "2405:b500::/32",
+          "2405:8100::/32",
+          "2a06:98c0::/29",
+          "2c0f:f248::/32",
+        ]
+      }
+    }
+    description = "Allow Cloudflare IPs - ipv6"
+  }
+
+  rule {
+    action   = "deny(403)"
+    priority = "2147483647"
+    match {
+      versioned_expr = "SRC_IPS_V1"
+      config {
+        src_ip_ranges = ["*"]
+      }
+    }
+    description = "default rule"
+  }
+}
