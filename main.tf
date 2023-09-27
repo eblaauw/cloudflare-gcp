7eb72c18eaf526531465caf0042462b903beea26
Edzo Blaauw <edzo@edzo.dev>
2023-09-27 08:16:35 +0000

    chore(): Update Cloudflare IP range

--- main.tf
+++ main.tf
@@ -23,32 +23,20 @@ resource "google_compute_security_policy" "policy" {
           "103.31.4.0/22",
           "141.101.64.0/18",
           "108.162.192.0/18",
-        ]
-      }
-    }
-    description = "Allow Cloudflare IPs - ipv4 (1)"
-  }
-  rule {
-    action   = "allow"
-    priority = "999"
-    match {
-      versioned_expr = "SRC_IPS_V1"
-      config {
-        src_ip_ranges = [
           "190.93.240.0/20",
           "188.114.96.0/20",
           "197.234.240.0/22",
           "198.41.128.0/17",
           "162.158.0.0/15",
-          "104.16.0.0/12",
+          "104.16.0.0/13",
+          "104.24.0.0/14",
           "172.64.0.0/13",
-          "131.0.72.0/22",
+          "131.0.72.0/22"
         ]
       }
     }
-    description = "Allow Cloudflare IPs - ipv4 (2)"
+    description = "Allow Cloudflare IPs - ipv4"
   }
-
 
   rule {
     action   = "allow"
@@ -63,7 +51,7 @@ resource "google_compute_security_policy" "policy" {
           "2405:b500::/32",
           "2405:8100::/32",
           "2a06:98c0::/29",
-          "2c0f:f248::/32",
+          "2c0f:f248::/32"
         ]
       }
     }
